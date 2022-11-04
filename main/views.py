d9035561eabb6d808c9aeaf4aa7a297177b12400
julamifra <julamifra@gmail.com>
2022-11-04 23:59:21 +0000

    add data in template

--- main/views.py
+++ main/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from .models import Item
 
 # Create your views here.
+
+
 def get_main(request):
-    return render(request, 'main/main.html')
+    items = Item.objects.all()
+    context = {
+        'items': items
+    }
+    return render(request, 'main/main.html', context)
     # return HttpResponse("<h1> Hello! </h1><p>This is a paragraph!!</p>")
