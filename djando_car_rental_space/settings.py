5cf5c9c79ff056145aa231c9e41e7e2696a8f3b5
julamifra <julamifra@gmail.com>
2022-11-05 15:07:15 +0000

    setup development enviroment

--- djando_car_rental_space/settings.py
+++ djando_car_rental_space/settings.py
@@ -29,7 +29,11 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-l%1c=p+rs+lq4u=alq#r#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = development
 
-ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME')]
+if development:
+    ALLOWED_HOSTS = ['localhost']
+else:
+    ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME')]
+
 # 'car-rental-space.herokuapp.com'
 
 
