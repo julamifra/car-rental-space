9eca48ee5fbbe3e6ad43cabf064f45fa1bf2b7af
julamifra <julamifra@gmail.com>
2022-11-05 15:15:50 +0000

    remove SECRET_KEY

--- djando_car_rental_space/settings.py
+++ djando_car_rental_space/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-l%1c=p+rs+lq4u=alq#r#7$)n_%i56*-yg^d=e2e1*_osq7=+y')
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = development
