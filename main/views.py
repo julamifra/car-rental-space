96a07dd0ac7f10dfd0883710d3df791902d85076
julamifra <julamifra@gmail.com>
2022-11-06 17:54:37 +0000

    finish CRUD

--- main/views.py
+++ main/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Item
+from .forms import ItemForm
 
 # Create your views here.
 
@@ -14,9 +15,42 @@ def get_main(request):
 
 def add_item(request):
     if request.method == 'POST':
-        name = request.POST.get('item_name')
-        done = 'done' in request.POST
-        Item.objects.create(name=name, done=done)
+        form = ItemForm(request.POST)
+        # name = request.POST.get('item_name')
+        # done = 'done' in request.POST
+        # Item.objects.create(name=name, done=done)
+        if form.is_valid():
+            form.save()
+            return redirect('get_main')
+    form = ItemForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'main/add_item.html', context)
 
-        return redirect('get_main')
-    return render(request, 'main/add_item.html')
+
+def edit_item(request, item_id):
+    item = get_object_or_404(Item, id=item_id)
+    if request.method == 'POST':
+        form = ItemForm(request.POST, instance=item)
+        if form.is_valid():
+            form.save()
+            return redirect('get_main')
+    form = ItemForm(instance=item)
+    context = {
+        'form': form
+    }
+    return render(request, 'main/edit_item.html', context)
+
+
+def toggle_item(request, item_id):
+    item = get_object_or_404(Item, id=item_id)
+    item.done = not item.done
+    item.save()
+    return redirect('get_main')
+
+
+def delete_item(request, item_id):
+    item = get_object_or_404(Item, id=item_id)
+    item.delete()
+    return redirect('get_main')
